fef414d3774fc12cb569bacbe9c2f66b0484ec3c
Ankit Pandey <114949005+ankitpandey88@users.noreply.github.com>
2025-09-13 17:24:39 +0530

    hh

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  build: {
-    outDir: 'dist', // Vercel expects the build output here
-  },
-  server: {
-    port: 3000, // Vercel uses this during dev preview
-    strictPort: true,
-  }
-});
+})
